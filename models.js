99e0a1a30820b61d7997d5eaf2dabee32b4725bc
anoopmundathan <anoop.mundathan@gmail.com>
2017-02-05 16:49:06 +0000

    Add pre save function to sort answers

--- models.js
+++ models.js
@@ -4,6 +4,18 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
+var sortAnswers = function(a, b) {
+	// -1 negative a before b
+	// 0 no change
+	// + positve a after b
+
+	if (a.votes === b.votes) {
+		return b.updateAt - a.updateAt;
+	}
+
+	return b.votes - a.votes;
+};
+
 var AnswerSchema = new Schema({
 	text: String,
 	createdAt: {
@@ -27,6 +39,11 @@ var QuestionSchema = new Schema({
 		default: Date.now
 	},
 	answers: [AnswerSchema]
+});
+
+QuestionSchema.pre('save', function(next) {
+	this.answers.sort(sortAnswers);
+	next();
 });
 
 var Question = mongoose.model('Question', QuestionSchema);
