b6381913fe7b64f3d979cd596f372cc2e8799040
anoopmundathan <anoop.mundathan@gmail.com>
2017-02-05 17:43:35 +0000

    Add instance methods

--- models.js
+++ models.js
@@ -32,6 +32,22 @@ var AnswerSchema = new Schema({
 	}
 });
 
+// Instance method
+AnswerSchema.method("update", function(updates, callback) {
+	Object.assign(this, updates, {updateAt: new Date()});
+	this.parent().save(callback);
+});
+
+// Instance method
+AnswerSchema.method("vote", function(vote, callback) {
+	if(vote === "up") {
+		this.votes += 1;
+	} else {
+		this.votes -= 1;
+	}
+	this.parent().save(callback);
+});
+
 var QuestionSchema = new Schema({
 	text: String,
 	createdAt: {
@@ -47,6 +63,7 @@ QuestionSchema.pre('save', function(next) {
 });
 
 var Question = mongoose.model('Question', QuestionSchema);
+
 module.exports.Question = Question;
 
 
